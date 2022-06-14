8977b06fa9e991fb2bb7842f66f10830cb4ea2b0
ajaykumar97 <ajay@diligentic.com>
2022-06-15 00:07:58 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,109 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  PermissionsAndroid,
+  Platform,
+  ActivityIndicator,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {BLEPrinter} from 'react-native-thermal-receipt-printer';
+
+const App = () => {
+  const [printers, setPrinters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPrinter, setCurrentPrinter] = useState();
+
+  const init = useCallback(async () => {
+    try {
+      if (Platform.OS === 'android') {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        );
+        console.log('granted: ', granted);
+      }
+      console.log('initializing');
+      await BLEPrinter.init();
+      console.log('initialized');
+      const printersOnNetwork = await BLEPrinter.getDeviceList();
+      console.log('printersOnNetwork: ', printersOnNetwork);
+      setPrinters(printersOnNetwork);
+      setLoading(false);
+    } catch (error) {
+      console.log('init BLE error: ', error);
+      alert('Scanning printers error');
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const _connectPrinter = async printer => {
+    try {
+      //connect printer
+      const selectedPrinter = await BLEPrinter.connectPrinter(
+        printer.inner_mac_address,
+      );
+      setCurrentPrinter(selectedPrinter);
+    } catch (error) {
+      console.log('connect printer error: ', error);
+    }
+  };
+
+  printBillTest = async () => {
+    try {
+      await BLEPrinter.printBill('<C>sample bill</C>', {beep: true});
+    } catch (error) {
+      console.log('print bill error: ', error);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={{backgroundColor: Colors.white, paddingTop: 10}}>
+          <Text style={styles.sectionTitle}>Printer App</Text>
+          {loading && <ActivityIndicator style={{marginVertical: 10}} />}
+          {printers.map(printer => (
+            <Pressable
+              key={printer.inner_mac_address}
+              style={{
+                borderWidth: 1,
+                paddingHorizontal: 10,
+                paddingVertical: 8,
+                marginTop: 10,
+              }}
+              onPress={() => _connectPrinter(printer)}>
+              <Text>{`device_name: ${printer.device_name}, inner_mac_address: ${printer.inner_mac_address}`}</Text>
+              {currentPrinter?.inner_mac_address ===
+                printer.inner_mac_address && (
+                <Text style={{color: 'green', fontSize: 16}}>Connected</Text>
+              )}
+            </Pressable>
+          ))}
+          {currentPrinter && (
+            <Pressable onPress={printBillTest}>
+              <Text>Print Bill Text</Text>
+            </Pressable>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+});
+
+export default App;
